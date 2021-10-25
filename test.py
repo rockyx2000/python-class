eb696d48d89ce6fae2d34e82cc45f34299bd3045
Hiroki Ohtake <hametu1992@gmail.com>
2021-10-25 13:51:10 +0900

    Edit Code

--- test.py
+++ test.py
@@ -12,13 +12,13 @@ for i in range(1,11):
     print(i)
     
 #%%
-greeting2 = "ばーか"
+greeting2 = "あいうえお"
 print(greeting2)
 
 text = input("好きな文字を入力してください")
 
 if text == "AAA":
-    print("あ？")
+    print("ああああ？")
     
 
 # %%
